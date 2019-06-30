3b642541289986be7f60669b537f23fdc43a1b09
Mesea <15201331135@163.com>
2019-06-30 17:51:58 +0800

    删除注释

--- src/App.js
+++ src/App.js
@@ -2,28 +2,6 @@ import React from 'react';
 import axios from "axios";
 import Route from "@/router"
 import Header from "@/components/layout/header"
-
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 class App extends React.Component{
   render(){
     return (
@@ -34,8 +12,7 @@ class App extends React.Component{
     )
   }
   componentDidMount(){
-    // ?
- 
+    
   }
 }
 
