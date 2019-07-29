6184c77b353cbc9a8b4d580c9539b3d0d7440e2f
Mesea <15201331135@163.com>
2019-07-29 11:52:30 +0800

    更新了打包流程_项目介绍

--- src/components/layout/header/header.jsx
+++ src/components/layout/header/header.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { NavLink  } from "react-router-dom";
 function App() {
    let toPathlist=[
-       {name:"信息页",path:"/Info"},
-       {name:"新闻页",path:"/News"},
-       {name:"测试页",path:"/Test"}
+       {name:"信息页",path:"/info"},
+       {name:"新闻页",path:"/news"},
+       {name:"测试页",path:"/test"}
    ] 
   return (
     <div className="App">
